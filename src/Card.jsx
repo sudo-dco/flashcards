30daa487ccfe67791e2335b590a9c62ef040eeac
Daniel Co <dcohomemail@gmail.com>
2021-02-18 17:32:10 -0800

    add app component

---
+++ src/Card.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+const Card = ({trivia, showAnswer, handleFlip, next}) => {
+    return (
+        <div className="card mb-3 border-primary">
+            <h5 className="card-header">{showAnswer ? "Answer" : "Question"}</h5>
+            <div className="card-body">
+                <p className="card-text">{showAnswer && trivia !== null ? trivia.answer : trivia.question}</p>
+                <button className="btn btn-primary me-2" onClick={handleFlip}>Flip Card</button>
+                <button className="btn btn-primary" onClick={next}>Next Card</button>
+            </div>
+        </div>
+    )
+}
+
+export default Card;
---
+++ src/Form.jsx
@@ -0,0 +1,59 @@
+import React, { useState } from "react";
+import axios from 'axios';
+
+const Form = ({ toast }) => {
+    const [questionInput, setQuestionInput] = useState("");
+    const [answerInput, setAnswerInput] = useState("");
+
+    const addQuestion = (e) => {
+        e.preventDefault();
+        axios.post("/add", {
+            question: questionInput,
+            answer: answerInput
+        });
+
+        setQuestionInput("");
+        setAnswerInput("");
+
+        toast();
+    };
+
+    const handleInput = ({ target }) => {
+        const { value } = target;
+
+        if (target.name === "question") {
+            setQuestionInput(value);
+        } else if (target.name ="answer") {
+            setAnswerInput(value);
+        }
+    };
+
+    return (
+        <form className="card border-primary" onSubmit={addQuestion}>
+            <h5 className="card-header">Add New Question:</h5>
+            <div className="card-body">
+                <label className="form-label mb-0" htmlFor="question">Question:</label>
+                <textarea
+                    className="form-control mb-2"
+                    name="question" 
+                    value={questionInput} 
+                    onChange={handleInput}
+                    rows="5"
+                    cols="35"
+                />
+                <label className="form-label mb-0" htmlFor="answer">Answer:</label>
+                <textarea
+                    className="form-control"
+                    name="answer" 
+                    value={answerInput} 
+                    onChange={handleInput}
+                    rows="5"
+                    cols="35"
+                />
+                <button className="btn btn-primary mt-2" type="submit">Submit</button>
+            </div>
+        </form>
+    )
+};
+
+export default Form;
