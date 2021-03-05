f39075ec9bd544fb9ce6e86b1d4846e5b5e775bb
Daniel Co <dcohomemail@gmail.com>
2021-03-04 19:13:29 -0800

    add function to add user and table to db

--- database/db.js
+++ database/db.js
@@ -8,6 +8,45 @@ const localDB = {
 
 const db = mysql.createConnection(process.env.JAWSDB_URL || localDB);
 
+const createTable = (username) => {
+    return new Promise((resolve, reject) => {
+        db.query(
+            `CREATE TABLE ${username}_trivia (id INT NOT NULL AUTO_INCREMENT PRIMARY KEY, question TEXT NOT NULL, answer TEXT NOT NULL)`,
+            (error, results) => {
+                if (error) {
+                    console.error(
+                        "Error creating trivia table for user: ",
+                        username,
+                        error
+                    );
+                    reject(error);
+                }
+                resolve(results);
+            }
+        );
+    });
+};
+
+const addUser = (username, password) => {
+    return new Promise((resolve, reject) => {
+        db.query(
+            "INSERT INTO users (username, password) VALUES (?, ?)",
+            [username, password],
+            (error, results) => {
+                if (error) {
+                    console.error(
+                        "Error adding user account: ",
+                        username,
+                        error
+                    );
+                    reject(error);
+                }
+                resolve(results);
+            }
+        );
+    });
+};
+
 const getCount = () => {
     return new Promise((resolve, reject) => {
         db.query(
@@ -87,6 +126,8 @@ const findById = (id) => {
 };
 
 module.exports = {
+    createTable,
+    addUser,
     getQuestion,
     addQuestion,
     getCount,
