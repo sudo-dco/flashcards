887f7820dc2ebf0d15a8babca2f7856fbbab8c04
Daniel Co <dcohomemail@gmail.com>
2021-03-10 17:19:29 -0800

    remove toast message

--- src/Home.jsx
+++ src/Home.jsx
@@ -3,31 +3,14 @@ import axios from "axios";
 import Card from "./Card.jsx"
 import Form from "./Form.jsx";
 
-function Home({ auth }) {
+function Home({ user, showToast }) {
     const [trivia, setTrivia] = useState(null);
     const [showAnswer, setShowAnswer] = useState(false);
-    const [message, setMessage] = useState("");
+    
 
     const getQuestion = async () => {
-        const result = await axios.get("/get");
+        const result = await axios.get(`/get/${user.name}`);
         setTrivia(result.data);
-    };
-
-    const showToast = (msg) => {
-        setMessage(`${msg}`);
-
-        const option = {
-            animation: true,
-            autohide: true,
-            delay: 2000
-        };
-
-        const toastElList = [].slice.call(document.querySelectorAll('.toast'))
-        const toastList = toastElList.map(function (toastEl) {
-            return new bootstrap.Toast(toastEl, option)
-        })
-
-        toastList[0].show();
     };
 
     const handleFlipButton = () => {
@@ -43,6 +26,7 @@ function Home({ auth }) {
             <div className="card m-3 text-center bg-secondary">
                 <h1 className="card-header text-white">Software Engineering Flash Cards</h1>
                 <div className="card-body bg-light">
+                    <p className="fs-6">Logged in as: {user.name}</p>
                     {
                         trivia === null ? 
                         "Loading..." : 
@@ -53,16 +37,7 @@ function Home({ auth }) {
                             next={getQuestion}
                         />
                     }
-                    <Form auth={auth} toast={showToast}/>
-                </div>
-                
-            </div>
-            <div className="toast hide align-items-center position-absolute m-5 top-0 end-0 bg-white border-primary" role="alert" aria-live="assertive" aria-atomic="true">
-                <div className="d-flex">
-                    <div className="toast-body">
-                        {message}
-                    </div>
-                    <button type="button" className="btn-close btn-close-white me-2 m-auto" data-bs-dismiss="toast" aria-label="Close"></button>
+                    <Form user={user} toast={showToast}/>
                 </div>
             </div>
         </div> 
