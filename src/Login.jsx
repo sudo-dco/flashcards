a0919998a6133ce5745435ca310d4f343bd6279d
Daniel Co <dcohomemail@gmail.com>
2021-03-10 17:20:21 -0800

    add bootstrap css and enter key listener

--- src/Login.jsx
+++ src/Login.jsx
@@ -18,7 +18,7 @@ const Login = ({ changeAuth }) => {
             })
             .then((response) => {
                 if (response.data.isAuthenticated) {
-                    changeAuth(true);
+                    changeAuth(response.data.user, response.data.isAuthenticated);
                     history.push("/home");
                 }
             })
@@ -40,13 +40,32 @@ const Login = ({ changeAuth }) => {
         }
     };
 
+    const handleEnter = (e) => {
+        if (e.code === "Enter") {
+            handleLogin(e);
+        };
+    };
+
     return (
-        <form onSubmit={handleLogin} onChange={handleInput}>
-            <h1>Flashcards Login:</h1>
-            <input type="text" name="username" placeholder="username" value={username} />
-            <input type="password" name="password" placeholder="password" value={password} />
-            <input type="submit" value="Login" />
-        </form>
+        <div className="h-100 d-flex align-items-center justify-content-center">
+            <div className="card">
+                <h1 className="card-header text-center">Flashcards Login</h1>
+                <form className="m-2" onSubmit={handleLogin} onKeyPress={handleEnter}>
+                    <div className="mb-3 ms-4 me-4">
+                        <label htmlFor="username" className="form-label mb-0">Username:</label>
+                        <input type="text"className="form-control" name="username" value={username} onChange={handleInput}/>
+                    </div>
+                    <div className="mb-3 ms-4 me-4">
+                        <label htmlFor="password" className="form-label mb-0">Password:</label>
+                        <input type="password" className="form-control" name="password" value={password} onChange={handleInput}/>
+                    </div>
+                    <div className="d-flex justify-content-between ms-4 me-4">
+                        <input type="submit" className="btn btn-primary text-center" value="Login" />
+                        <input type="button" className="btn btn-primary" value="Create Account" onClick={() => {history.push("/signup")}}/>
+                    </div>
+                </form>
+            </div>
+        </div>
     )
 }
 
