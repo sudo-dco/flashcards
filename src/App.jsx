309f6c37be5bc0bc5ece0c544da9266c464ce17e
Daniel Co <dcohomemail@gmail.com>
2021-03-10 17:18:26 -0800

    move toast message from home to app component

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,9 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import {
     Switch,
-    Route
+    Route,
+    useLocation,
+    useHistory,
 } from "react-router-dom";
 import Home from "./Home.jsx";
 import Login from "./Login.jsx";
@@ -9,26 +11,70 @@ import Signup from "./Signup.jsx";
 
 
 function App() {
-    const [isAuth, setIsAuth] = useState(false);
+    const [user, setUser] = useState({
+        name: null,
+        isAuth: false,
+    });
+    const [message, setMessage] = useState("");
 
-    const changeAuth = (value) => {
-        if (value === true) {
-            setIsAuth(true);
+    const history = useHistory();
+    const location = useLocation();
+
+    useEffect(() => {
+        if (user.name === null && location.pathname === "/home") {
+            history.push("/login");
+        }
+    })
+
+    const changeAuth = (name, status) => {
+        if (status === true) {
+            setUser({
+                ...user,
+                name: name,
+                isAuth: status,
+            });
         }
     }
 
+    const showToast = (msg) => {
+        setMessage(`${msg}`);
+
+        const option = {
+            animation: true,
+            autohide: true,
+            delay: 2000
+        };
+
+        const toastElList = [].slice.call(document.querySelectorAll('.toast'))
+        const toastList = toastElList.map(function (toastEl) {
+            return new bootstrap.Toast(toastEl, option)
+        })
+
+        toastList[0].show();
+    };
+
     return (
+        <div className="h-100">
             <Switch>
                 <Route path="/signup">
-                    <Signup />
+                    <Signup showToast={showToast} />
                 </Route>
                 <Route path="/home">
-                    <Home auth={isAuth} />
+                    <Home user={user} showToast={showToast} />
                 </Route>
                 <Route path="/">
                     <Login changeAuth={changeAuth} />
                 </Route>
             </Switch>
+            <div className="toast hide align-items-center position-absolute m-4 top-0 end-0 bg-white border-primary" role="alert" aria-live="assertive" aria-atomic="true">
+                <div className="d-flex">
+                    <div className="toast-body">
+                        {message}
+                    </div>
+                    <button type="button" className="btn-close btn-close-white me-2 m-auto" data-bs-dismiss="toast" aria-label="Close"></button>
+                </div>
+            </div>
+        </div> 
     )
 }
 
