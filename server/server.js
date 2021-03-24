4158a9b02dc48abe9418a8e880d8548be81f0eac
Daniel Co <dcohomemail@gmail.com>
2021-03-23 23:12:28 -0700

    add delete route

--- server/server.js
+++ server/server.js
@@ -1,5 +1,6 @@
+require("dotenv").config();
 const express = require("express");
-const session = require("express-session");
+// const session = require("express-session");
 const path = require("path");
 const passport = require("passport");
 const LocalStrategy = require("passport-local").Strategy;
@@ -8,12 +9,11 @@ const db = require("../database/db");
 const app = express();
 const port = process.env.PORT || 3000;
 
-const signUpCode = "testcode1234";
+const signUpCode = process.env.SIGNUP_CODE;
 const userQuestionList = {};
 
 const generateIdList = async (username) => {
     const ids = [];
-
     const count = await db.getCount(username);
 
     for (let i = 1; i <= count.value; i++) {
@@ -26,7 +26,6 @@ const generateIdList = async (username) => {
 const getQuestionId = async (username) => {
     // create array of ids in user's trivia table
     // pull ids out of array until there's no more left and then recreate array
-
     if (
         !userQuestionList[username] ||
         userQuestionList[username].length === 0
@@ -37,17 +36,18 @@ const getQuestionId = async (username) => {
     const randomIndex = Math.floor(
         Math.random() * (userQuestionList[username].length - 0 + 1) + 0
     );
-
     const id = userQuestionList[username].splice(randomIndex, 1);
 
     return id[0];
 };
 
 app.use(
-    session({
-        secret: "testsecret1234",
+    db.session({
+        name: "flashcards.sid",
+        secret: process.env.SESSION_SECRET,
         resave: false,
         saveUninitialized: true,
+        store: db.sessionStore,
     })
 );
 app.use(passport.initialize());
@@ -55,30 +55,56 @@ app.use(passport.session());
 app.use(express.json());
 app.use(express.static("public"));
 
-app.get("/get", async (req, res) => {
-    const { username } = req.user;
-    console.log("get route user obj: ", username);
+app.get("/question", async (req, res) => {
+    let result = null;
 
-    // add check for existing username
-
-    if (username) {
+    if (req.user) {
+        const { username } = req.user;
         const number = await getQuestionId(username);
         const question = await db.getQuestion(username, number);
-        res.status(200).send(question[0]);
+        result = {
+            id: question[0][id],
+            question: question[0][question],
+            answer: question[0][answer],
+        };
+    } else {
+        result = "Not authorized, please login again.";
     }
+
+    res.status(200).send(result);
 });
 
-app.post("/add", (req, res) => {
-    const { username } = req.user;
-    const { user } = req.params;
-    const { question, answer } = req.body;
+app.get("/question/all", async (req, res) => {});
 
-    console.log("add route user obj: ", username);
+app.post("/question", (req, res) => {
+    const { username } = req.user;
+    const { question, answer } = req.body;
 
     if (username) {
         db.addQuestion(username, question, answer);
-        res.sendStatus(200);
     }
+
+    res.sendStatus(200);
+});
+
+app.delete("/question", (req, res) => {
+    const { id } = req.body;
+    let result = null;
+
+    if (req.user) {
+        const { username } = req.user;
+        try {
+            db.deleteQuestion(username, id);
+            result = "Question Deleted";
+        } catch (error) {
+            console.error("Error deleting question from DB: ", error);
+            result = "Error deleting question from DB";
+        }
+    } else {
+        result = "Not authorized, please login again.";
+    }
+
+    res.status(200).send(result);
 });
 
 app.post("/login", passport.authenticate("local"), (req, res) => {
@@ -88,13 +114,15 @@ app.post("/login", passport.authenticate("local"), (req, res) => {
 });
 
 app.get("/checkAuth", (req, res) => {
-    if (req.user) {
-        res.send({
+    if (req.session.passport) {
+        const { username } = req.user;
+
+        res.json({
             user: username,
             isAuthenticated: true,
         });
     } else {
-        res.send({ isAuthenticated: false });
+        res.json({ isAuthenticated: false });
     }
 });
 
